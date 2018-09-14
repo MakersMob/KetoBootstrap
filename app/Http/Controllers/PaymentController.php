66f74363312eeea1c7a4156668c7f6d2065ee9a3
Scrivs <scrivs@gmail.com>
2018-09-14 13:28:30 -0700

    init

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -11,7 +11,6 @@ class PaymentController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request);
         switch ($request->product) {
             case 'general':
                 $amount = 47;
