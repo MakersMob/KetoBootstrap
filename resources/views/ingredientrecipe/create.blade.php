ca5670ed42d2b6eb48fdc0626907d97270fa0f28
Scrivs <scrivs@gmail.com>
2018-09-18 12:02:29 -0700

    init

--- resources/views/ingredientrecipe/create.blade.php
+++ resources/views/ingredientrecipe/create.blade.php
@@ -47,6 +47,7 @@
 											<option value=""></option>
 											<option value="bottles">bottles</option>
 											<option value="cans">cans</option>
+											<option value="capsules">capsules</option>
 											<option value="clove">clove</option>
 											<option value="cubes">cubes</option>
 											<option value="cup">cup</option>
