c72d340ebfef84e53e3d13cbb48d495f904e197c
Scrivs <scrivs@gmail.com>
2018-09-25 08:58:12 -0700

    init

--- resources/views/7daymealplan.blade.php
+++ resources/views/7daymealplan.blade.php
@@ -16,52 +16,296 @@
 		<div class="row">
 			<div class="col-12">
 				<h2>Day 1</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/cinnamon-roll-waffle">Cinnamon Roll Waffle</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></li>
-					<li>Snack: <a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/cinnamon-roll-waffle">Cinnamon Roll Waffle</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
 				<h2>Day 2</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
 				<h2>Day 3</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a> (Eat 2)</li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></li>
-					<li>Snack or Dessert: <a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a> (Eat 2)</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack or Dessert
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
 				<h2>Day 4</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
 				<h2>Day 5</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/portobello-pizza">Portobello Pizza</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
-					<li>Snack or Dessert: <a href="https://ketobootstrap.com/recipe/microwave-brownies">Microwave Brownies</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/portobello-pizza">Portobello Pizza</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack or Dessert
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/microwave-brownies">Microwave Brownies</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
 				<h2>Day 6</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></li>
-					<li>Snack or Dessert: <a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></li>
-				</ul>
-				<h2>Day 7</h2>
-				<ul class="list-unstyled">
-					<li>Breakfast: <a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketobootstrap.com/recipe/portobello-pizza">Portobello Pizza</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
-					<li>Snack or Dessert: <a href="https://ketobootstrap.com/recipe/microwave-brownies">Microwave Brownies</a></li>
-				</ul>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/breakfast-quiche">Breakfast Quiche</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/zucchini-poppers">Zucchini Poppers</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Sheppard's Pie</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketodash.com/recipe/chocolate-chip-cookie-cheesecake-bars">Chocolate Chip Cookie Cheesecake Bars</a></h5>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<h2>Day 5</h2>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Breakfast
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Lunch
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/portobello-pizza">Portobello Pizza</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Dinner
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-lg-3">
+				<div class="card recipe">
+					<div class="card-header">
+						Snack or Dessert
+					</div>
+					<div class="card-body">
+						<h5 class="card-title"><a href="https://ketobootstrap.com/recipe/microwave-brownies">Microwave Brownies</a></h5>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
