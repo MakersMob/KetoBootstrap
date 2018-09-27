18bb9958abff40af2e0dc2fead332340a92ff838
Scrivs <scrivs@gmail.com>
2018-09-27 13:02:15 -0700

    init

--- resources/views/shopping-list.blade.php
+++ resources/views/shopping-list.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => 'Keto Shopping List'])
+@extends('layouts.app', ['title' => 'Keto Grocery List', 'description' => 'Wondering what foods you can buy at the grocery store for the ketogenic diet? Check out this keto shopping list.'])
 
 @section('content')
 <section class="welcome food">
