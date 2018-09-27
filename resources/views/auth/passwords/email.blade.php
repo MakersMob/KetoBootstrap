6b32798853b2b040c83e3c091bea5ac901f9d357
Scrivs <scrivs@gmail.com>
2018-09-27 16:49:54 -0700

    inti

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="welcome smiles">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="linewrap"><span>Reset Your Password</span></h1>
+            </div>
+        </div>
+    </div>
+</section>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
