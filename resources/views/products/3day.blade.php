a0de8d78e6d5a15e9c5cd374b48679fdb1f1d296
Scrivs <scrivs@gmail.com>
2018-10-02 07:11:10 -0700

    init

--- resources/views/products/3day.blade.php
+++ resources/views/products/3day.blade.php
@@ -7,7 +7,7 @@
 			<div class="col-12">
 				<h1 class="linewrap"><span>The 3-Day Weight Loss Manual</span></h1>
 				<h2 class="subheader linewrap"><span>Learn About the Ketogenic Diet and Follow a Routine That Will Help You Achieve Ketosis and Weight Loss in Just 3 Days!</span></h2>
-				<p class="text-center" style="margin-top: 2rem;"><a data-dpd-type="button" data-variant="price-right" data-text="PURCHASE NOW" data-button-size="dpd-large" data-bg-color="ff6600" data-bg-color-hover="ff7b0b" data-text-color="ffffff" data-pr-bg-color="ffffff" data-pr-color="000000" data-lightbox="1" href="https://keto-dash.dpdcart.com/cart/add?product_id=155405&amp;method_id=166224">PURCHASE NOW</a><script src="https://keto-dash.dpdcart.com/dpd.js"></script></p>
+				<p class="" style="margin-top: 2rem;"><a data-dpd-type="button" data-variant="price-right" data-text="PURCHASE NOW" data-button-size="dpd-large" data-bg-color="ff6600" data-bg-color-hover="ff7b0b" data-text-color="ffffff" data-pr-bg-color="ffffff" data-pr-color="000000" data-lightbox="1" href="https://keto-dash.dpdcart.com/cart/add?product_id=155405&amp;method_id=166224">PURCHASE NOW</a><script src="https://keto-dash.dpdcart.com/dpd.js"></script></p>
 			</div>
 		</div>
 	</div>
@@ -22,7 +22,7 @@
 				<p>This 119-page book will introduce you to Keto and provide you with a plan that will help you achieve Ketosis in 3 days.</p>
 				<p>Want to give Keto a shot over the weekend? This is your chance.</p>
 				<p>Comes with 20 bonus keto-friendly recipes that will help you experience keto to its fullest.</p>
-				<p class="text-center" style="margin-top: 2rem;"><a data-dpd-type="button" data-variant="price-right" data-text="PURCHASE NOW" data-button-size="dpd-large" data-bg-color="ff6600" data-bg-color-hover="ff7b0b" data-text-color="ffffff" data-pr-bg-color="ffffff" data-pr-color="000000" data-lightbox="1" href="https://keto-dash.dpdcart.com/cart/add?product_id=155405&amp;method_id=166224">PURCHASE NOW</a><script src="https://keto-dash.dpdcart.com/dpd.js"></script></p>
+				<p class="" style="margin-top: 2rem;"><a data-dpd-type="button" data-variant="price-right" data-text="PURCHASE NOW" data-button-size="dpd-large" data-bg-color="ff6600" data-bg-color-hover="ff7b0b" data-text-color="ffffff" data-pr-bg-color="ffffff" data-pr-color="000000" data-lightbox="1" href="https://keto-dash.dpdcart.com/cart/add?product_id=155405&amp;method_id=166224">PURCHASE NOW</a><script src="https://keto-dash.dpdcart.com/dpd.js"></script></p>
 			</div>
 			<div class="col-12 col-lg-5">
 				<div class="image">
