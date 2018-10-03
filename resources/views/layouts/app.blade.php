77d74226dbcbc4376c46003d2dfa90687f5f9ab5
Scrivs <scrivs@gmail.com>
2018-10-03 13:57:39 -0700

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,16 +34,9 @@
     <div id="app">
         <nav class="navbar fixed-top navbar-expand-lg navbar-light">
             <div class="container">
-                @auth
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        <img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2018/08/09032004/logo-64.png" alt="Keto Bootstrap"> Keto Bootstrap
-                    </a>
-                @endauth
-                @guest
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2018/08/09032004/logo-64.png" alt="Keto Bootstrap"> Keto Bootstrap
-                    </a>
-                @endguest
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="https://s3-us-west-2.amazonaws.com/ketodash-master/blog/wp-content/uploads/2018/08/09032004/logo-64.png" alt="Keto Bootstrap"> Keto Bootstrap
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
