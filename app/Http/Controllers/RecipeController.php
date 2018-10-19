6248d71ea417807f7453c4a816f44d4e077ff3db
Scrivs <scrivs@gmail.com>
2018-10-19 07:57:19 -0700

    init

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -130,9 +130,11 @@ class RecipeController extends Controller
         $tags = explode(",", $request->tags);
 
         foreach($tags as $tag) {
-            $t = Tag::firstOrCreate(['tag' => strtolower(trim($tag))]);
+            if(!empty($tag)) {
+                $t = Tag::firstOrCreate(['tag' => strtolower(trim($tag))]);
 
-            $recipe->tags()->attach($t->id);
+                $recipe->tags()->attach($t->id);
+            }
         }
 
         return redirect('recipe/'.$recipe->slug);
