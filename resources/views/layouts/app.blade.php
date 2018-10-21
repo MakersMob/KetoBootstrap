8cc9f26c443418065bf0e88d35c0661a7e6435b2
Scrivs <scrivs@gmail.com>
2018-10-21 14:46:15 -0700

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,6 +143,22 @@
 
           gtag('config', 'UA-51364-104');
         </script>
+
+        <!-- Drip -->
+        <script type="text/javascript">
+          var _dcq = _dcq || [];
+          var _dcs = _dcs || {};
+          _dcs.account = '9033474';
+
+          (function() {
+            var dc = document.createElement('script');
+            dc.type = 'text/javascript'; dc.async = true;
+            dc.src = '//tag.getdrip.com/9033474.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(dc, s);
+          })();
+        </script>
+        <!-- end Drip -->
     @endif
 </body>
 </html>
