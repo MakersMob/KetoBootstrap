f4dc7b1b30c4f41a92d5fb7a1c9cdd99d1c5b347
Scrivs <scrivs@gmail.com>
2018-10-21 15:12:05 -0700

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app', ['title' => 'Helping You Bootstrap the Keto Diet'])
 
 @section('content')
-<section class="welcome food">
+<section class="welcome food3">
   <div class="container">
     <div class="row">
         <div class="col-12">
             <h1 class="billboard linewrap"><span>Start off the right way with the Keto Diet</span></h1>
+            <h2 class="subheader linewrap"><span>Helping you start keto, stick to keto, and love keto</span></h2>
         </div>
     </div>
   </div>
@@ -19,7 +20,7 @@
 			<div class="col-12 col-lg-8">
 				<p>Maybe you feel you can look better. Maybe you feel you should smile when you see yourself. Maybe you feel like you deserve more for yourself.</p>
 				<p>Basically, you're tired of where you're at now and you know you want to be at a different phase with your body.</p>
-				<p>That is where the <strong>ketogenic diet</strong> comes into play.</p>
+				<p>That is where the <a href="/keto-diet"><strong>ketogenic diet</strong></a> comes into play.</p>
 				<p>Now, if you don't know about keto then I can understand that the word <em>diet</em> is a pretty shitty term. It usually means eating things that you aren't happy about.</p>
 				<p>The Keto Bootstrap Team is here to help you navigate the Ketogenic Diet. You can do it the easy way, you can do it the hard way, or you can <strong>do it YOUR way</strong>.</p>
 			</div>
