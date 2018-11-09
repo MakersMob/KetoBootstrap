567b29edbd7a8db6fb76df7c7bfa02889c917ce0
Scrivs <scrivs@gmail.com>
2018-11-08 19:12:53 -0800

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,7 +125,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <p>&copy; Makers Mob LLC. | <a href="/keto-diet">Ketogenic Diet Guide</a> | <a href="/keto-shopping-list">Keto Shopping List</a> | <a href="/blog">Keto Blog</a> | <a href="/keto-meal-plan">Keto Diet Menu</a></p>
+                        <p>&copy; Makers Mob LLC. | <a href="/keto-diet">Ketogenic Diet Guide</a> | <a href="/keto-shopping-list">Keto Shopping List</a> | <a href="/blog">Keto Blog</a> | <a href="/keto-meal-plan">Keto Diet Menu</a> | <a href="/privacy-policy">Privacy Policy</a></p>
                     </div>
                 </div>
             </div>
