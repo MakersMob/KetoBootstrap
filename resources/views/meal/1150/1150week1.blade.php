3eaffad9ca1331fe5beb9841f318bc5bcc00e7bf
Scrivs <scrivs@gmail.com>
2019-11-17 06:57:27 -0800

    recipe fix

--- resources/views/meal/1150/1150week1.blade.php
+++ resources/views/meal/1150/1150week1.blade.php
@@ -16,45 +16,45 @@
 			<div class="col-12 col-lg-8">
 				<h2>Day 1</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/cinnamon-roll-waffle">Cinnamon Roll Waffle</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/zucchini-poppers">Zucchini Poppers</a> (eat 2)</li>
-					<li>Dinner: <a href="https://ketodash.com/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></li>
+					<li>Breakfast: <a href="/recipe/cinnamon-roll-waffle">Cinnamon Roll Waffle</a></li>
+					<li>Lunch: <a href="/recipe/zucchini-poppers">Zucchini Poppers</a> (eat 2)</li>
+					<li>Dinner: <a href="/recipe/one-pot-shrimp-alfredo">One Pot Shrimp Alfredo</a></li>
 				</ul>
 				<h2>Day 2</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/zucchini-poppers">Zucchini Poppers</a> (eat 2)</li>
+					<li>Breakfast: <a href="/recipe/breakfast-quiche">Breakfast Quiche</a></li>
+					<li>Lunch: <a href="/recipe/zucchini-poppers">Zucchini Poppers</a> (eat 2)</li>
 					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Shepherd's Pie</a></li>
 				</ul>
 				<h2>Day 3</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/portobello-pizza">Portobello Pizza</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
+					<li>Breakfast: <a href="/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
+					<li>Lunch: <a href="/recipe/portobello-pizza">Portobello Pizza</a></li>
+					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
 				</ul>
 				<h2>Day 4</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
+					<li>Breakfast: <a href="/recipe/breakfast-quiche">Breakfast Quiche</a></li>
+					<li>Lunch: <a href="/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
 					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Shepherd's Pie</a></li>
 				</ul>
 				<h2>Day 5</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/portobello-pizza">Portobello Pizza</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
+					<li>Breakfast: <a href="/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
+					<li>Lunch: <a href="/recipe/portobello-pizza">Portobello Pizza</a></li>
+					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
 				</ul>
 				<h2>Day 6</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/breakfast-quiche">Breakfast Quiche</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
+					<li>Breakfast: <a href="/recipe/breakfast-quiche">Breakfast Quiche</a></li>
+					<li>Lunch: <a href="/recipe/avocado-taco-boats">Avocado Taco Boats</a></li>
 					<li>Dinner: <a href="https://ketobootstrap.com/recipe/southwest-shepherd-s-pie">Southwest Shepherd's Pie</a></li>
 				</ul>
 				<h2>Day 7</h2>
 				<ul>
-					<li>Breakfast: <a href="https://ketodash.com/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
-					<li>Lunch: <a href="https://ketodash.com/recipe/portobello-pizza">Portobello Pizza</a></li>
-					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="https://ketodash.com/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
+					<li>Breakfast: <a href="/recipe/cream-cheese-scrambled-eggs">Cream Cheese Scrambled Egg (eat both servings)</a></li>
+					<li>Lunch: <a href="/recipe/portobello-pizza">Portobello Pizza</a></li>
+					<li>Dinner: <a href="https://ketobootstrap.com/recipe/italian-sausage-pepper-jack-meatballs">Italian Sausage &amp; Pepperjack Meatballs</a>, <a href="/recipe/loaded-cauliflower-mashed-potatoes">Loaded Cauliflower Mashed Potatoes</a></li>
 				</ul>
 			</div>
 			<div class="col-12 col-lg-4">
